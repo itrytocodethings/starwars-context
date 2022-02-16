6c44d3122ad0587eca0890afa5d001d6415ad6ca
itrytocodethings <imwaynebailey@gmail.com>
2022-02-16 18:21:15 -0500

    updated card props

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,28 +1,24 @@
 import React from 'react';
 
-export const Card = (props) => {
-    let currentEntity;
-    props.type == 'character' ? currentEntity = {...props.character} : currentEntity = {...props.planet}
-    console.log(currentEntity);
-    let imgURL = props.character ? 'https://www.sideshow.com/storage/product-images/2172/r2-d2-deluxe_star-wars_feature.jpg' : 'https://wallpaperaccess.com/full/1251069.jpg'
-    // console.log(props.character) //question for george, why does props.character.name not work?? why do I have to spread to a new obj????
+export const Card = ({type, character, planet}) => {
+    let imgURL = character ? 'https://www.sideshow.com/storage/product-images/2172/r2-d2-deluxe_star-wars_feature.jpg' : 'https://wallpaperaccess.com/full/1251069.jpg'
     return (
     <div className="card" style={{width: '25rem'}}>
     <img className="card-img-top" src={imgURL} alt="Card image cap" />
     <div className="card-body">
-        <h5 className="card-title">{currentEntity.name}</h5>
-        {props.type == "character" ? 
+        <h5 className="card-title">{type == 'character' ? character.name : planet.name}</h5>
+        {type == "character" ? 
         (        
             <div>
-                <p>Hair Color: {currentEntity.hair_color}</p>
-                <p>Eye Color: {currentEntity.eye_color}</p>
+                <p>Hair Color: {character.hair_color}</p>
+                <p>Eye Color: {character.eye_color}</p>
             </div>
         ) 
         :
         (
             <div>
-                <p>Population: {currentEntity.population}</p>
-                <p>Terrain: {currentEntity.terrain}</p>
+                <p>Population: {planet.population}</p>
+                <p>Terrain: {planet.terrain}</p>
             </div>
         )
         }
