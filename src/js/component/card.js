af7ab36a8807f6d5efb6d7d404d4995d0f5d02a2
itrytocodethings <imwaynebailey@gmail.com>
2022-02-18 17:42:06 -0500

    css updates

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const Card = ({ type, character, planet }) => {
     let imgURL = character ? 'https://www.sideshow.com/storage/product-images/2172/r2-d2-deluxe_star-wars_feature.jpg' : 'https://wallpaperaccess.com/full/1251069.jpg'
     return (
-        <div className="card bg-dark" style={{ width: '25rem' }}>
+        <div className="card bg-dark mb-1" style={{ width: '25rem' }}>
             <img className="card-img-top" src={imgURL} alt="Card image cap" />
             <div className="card-body">
                 <h5 className="card-title">{character ? character.name : planet.name}</h5>
