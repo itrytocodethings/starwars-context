6bfa208a64ca15ee65276170036f88a57dc36de0
itrytocodethings <wack@LAPTOP-PT3CPMRQ.localdomain>
2022-02-18 18:46:27 -0500

    cleaned up card component

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Card = ({character, planet }) => {
+export const Card = ({ character, planet }) => {
     let imgURL = character ? 'https://www.sideshow.com/storage/product-images/2172/r2-d2-deluxe_star-wars_feature.jpg' : 'https://wallpaperaccess.com/full/1251069.jpg'
     return (
         <div className="card bg-dark mb-1" style={{ width: '25rem' }}>
             <img className="card-img-top" src={imgURL} alt="Card image cap" />
             <div className="card-body">
                 <h5 className="card-title">{character ? character.name : planet.name}</h5>
-                {character ?
-                    (
-                        <div>
-                            <p>Hair Color: {character.hair_color}</p>
-                            <p>Eye Color: {character.eye_color}</p>
-                        </div>
-                    )
-                    :
-                    (
-                        <div>
-                            <p>Population: {planet.population}</p>
-                            <p>Terrain: {planet.terrain}</p>
-                        </div>
-                    )
-                }
+                <div>
+                    <p>{character ? `Hair Color: ${character.hair_color}` : `Population: ${planet.population}`}</p>
+                    <p>{character ? `Eye Color: ${character.eye_color}` : `Terrain: ${planet.terrain}`}</p>
+                </div>
                 <Link
                     to={{
                         pathname: `details/${character ? character.name : planet.name}`,
